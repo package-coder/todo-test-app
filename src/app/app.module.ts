fb765d74303cad7afc21e64c237ae8adadff0c35
Christian <pujado.chris@gmail.com>
2023-10-21 18:12:24 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoCardComponent } from './todo/todo-card/todo-card.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { Routes, RouterModule, Router } from '@angular/router';
+import { AddTodoDrawerComponent } from './todo/add-todo-drawer/add-todo-drawer.component';
 
+
+const routes: Routes = [
+  
+  { path: 'todos/add', component: AddTodoDrawerComponent },
+]
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+    TodoCardComponent,
+    TodoListComponent,
+    AddTodoDrawerComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/todo/add-todo-drawer/add-todo-drawer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo-drawer',
+  templateUrl: './add-todo-drawer.component.html',
+  styleUrls: ['./add-todo-drawer.component.css']
+})
+export class AddTodoDrawerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
