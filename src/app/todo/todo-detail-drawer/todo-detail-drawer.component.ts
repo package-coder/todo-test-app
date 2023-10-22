55d58632655b8070fdf5e2651357abded35d9034
Christian <pujado.chris@gmail.com>
2023-10-23 04:09:41 +0800

    Fix todo detail form

--- src/app/todo/todo-detail-drawer/todo-detail-drawer.component.ts
+++ src/app/todo/todo-detail-drawer/todo-detail-drawer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormArray, Validators, FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as moment from 'moment';
 import { TaskService } from 'src/services/task.service';
 import { TodoService } from 'src/services/todo.service';
 
@@ -11,10 +12,21 @@ import { TodoService } from 'src/services/todo.service';
 })
 export class TodoDetailDrawerComponent implements OnInit {
 
-  todo!: any;
   loading: boolean = false;
   submitting: boolean = false;
-  form!: FormGroup;
+
+  todo: any
+  form = new FormGroup({
+    tasks: new FormArray<FormControl>([], {
+      validators: [Validators.minLength(1)]
+    }),
+    tagSelector: new FormControl(),
+    archivedTasks: new FormArray<FormControl>([], {
+      validators: [Validators.minLength(1)]
+    }),
+    taskInput: new FormControl(),
+    name: new FormControl('', [Validators.required])
+  });
 
   toggleTodoArchive: boolean = false;
   showArchivedTasks: boolean = false;
@@ -25,70 +37,92 @@ export class TodoDetailDrawerComponent implements OnInit {
     this.route.params.subscribe(params => this.fetchTodo(Number(params['id'])));
   }
 
-  initForm() {
-    const tasks = this.todo?.tasks.filter((task: any) => !task.isArchived)?.map((task: any) => new FormControl(task))
-    const archivedTasks = this.todo?.tasks.filter((task: any) => task.isArchived)?.map((task: any) => new FormControl(task))
+  updateForm(data: any) {
+    const tasksForm = this.form.get('tasks') as FormArray
+    const archivedTasksForm = this.form.get('archivedTasks') as FormArray
 
-    this.form = new FormGroup({
-      tasks: new FormArray(tasks, {
-        validators: [Validators.minLength(1)]
-      }),
-      archivedTasks: new FormArray(archivedTasks, {
-        validators: [Validators.minLength(1)]
-      }),
-      task: new FormControl(),
-      name: new FormControl(this.todo?.name, [Validators.required])
-    })
+    const tasks = data.tasks.filter((task: any) => !task.isArchived)
+    const archivedTasks = data.tasks.filter((task: any) => task.isArchived)
 
+    tasks.forEach((task: any) => tasksForm?.push(new FormControl(task)))
+    archivedTasks.forEach((task: any) => archivedTasksForm?.push(new FormControl(task)))
+
+    this.form.get('name')?.setValue(data.name)
+    this.form.get('tagSelector')?.setValue({ values: data.tags })
+    this.todo = {
+      ...data,
+      createdAt: moment(data?.createdAt).format('dddd, MMMM DD')
+    }
   }
 
   fetchTodo(todoId: number) {
     this.loading = true
-    this.todoService.getTodo(todoId)
+    this.todoService?.getTodo(todoId)
       .subscribe(
-        data => this.todo = data,
+        data => this.updateForm(data),
         null,
-        () => {
-          this.loading = false
-          this.initForm()
-        }
+        () => this.loading = false
       )
   }
 
   addTask(e: any) {
+    const tasks = this.form.get('tasks') as FormArray
+    const taskInput = this.form.get('taskInput') as FormControl
+    const todoId = Number(this.route.snapshot.params['id'])
+
     if (e?.keyCode == 13) {
       e?.preventDefault()
+
       const value = e?.target?.value;
       if (!value) return;
 
-      this.form.get('tasks')?.value?.push({ name: value, isCompleted: false })
-      this.form.get('task')?.setValue('')
+      tasks.push(new FormControl({ name: value, isCompleted: false }))
+      taskInput.setValue('')
 
-      this.todoService.addTaskToTodo(this.todo.id, { name: value, isCompleted: false }).subscribe()
+      this.todoService
+        .addTaskToTodo(todoId, { name: value, isCompleted: false })
+        .subscribe(() => this.fetchTodo(todoId))
     }
   }
 
   completeTask(index: number) {
-    const task = this.todo?.tasks[index]
-      task.isCompleted = !task.isCompleted
-      this.initForm()
-      this.taskService.updateTask(task?.id, { ...task }).subscribe()
+    const tasks = this.form.get('tasks') as FormArray
+    const task = tasks.at(index).value
+
+    task.isCompleted = !task.isCompleted
+    this.taskService
+      .updateTask(task.id, { ...task })
+      .subscribe()
   }
 
   archiveTask(index: number) {
-    const task = this.form.get('tasks')?.value[index]
-    task.isArchived = true
-    this.initForm()
+    const tasks = this.form.get('tasks') as FormArray
+    const taskControl = tasks.at(index)
+    const task = taskControl.value
 
-    this.taskService.updateTask(task?.id, { ...task }).subscribe()
+    task.isArchived = true
+    this.taskService
+      .updateTask(task.id, { ...task })
+      .subscribe(() => {
+        const archivedTasks = this.form.get('archivedTasks') as FormArray
+        archivedTasks.push(taskControl)
+        tasks.removeAt(index)
+      })
   }
 
   restoreTask(index: number) {
-    const task = this.form.get('archivedTasks')?.value[index]
-    task.isArchived = false
-    this.initForm()
+    const archivedTasks = this.form.get('archivedTasks') as FormArray
+    const taskControl = archivedTasks.at(index)
+    const task = taskControl.value
 
-    this.taskService.updateTask(task?.id, { ...task }).subscribe()
+    task.isArchived = false
+    this.taskService
+      .updateTask(task.id, { ...task })
+      .subscribe(() => {
+        const tasks = this.form.get('tasks') as FormArray
+        tasks.push(taskControl)
+        archivedTasks.removeAt(index)
+      })
   }
 
   handleToggleArchive() {
@@ -96,7 +130,9 @@ export class TodoDetailDrawerComponent implements OnInit {
   }
 
   handleArchiveTodo() {
-    this.todoService.updateTodo(this.todo?.id, { ...this.todo, isArchived: true })
+    const todoId = Number(this.route.snapshot.params['id'])
+
+    this.todoService.updateTodo(todoId, { ...this.todo, isArchived: true })
       .subscribe(
         () => {
           this.handleToggleArchive()
@@ -105,7 +141,7 @@ export class TodoDetailDrawerComponent implements OnInit {
       )
   }
 
-  toggleArchivedTasks () {
+  toggleArchivedTasks() {
     this.showArchivedTasks = !this.showArchivedTasks
   }
 
