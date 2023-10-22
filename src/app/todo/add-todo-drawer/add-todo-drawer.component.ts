a7f627faaaf3d8c814980412b01a67ccea617323
Christian <pujado.chris@gmail.com>
2023-10-23 04:09:25 +0800

    Fix add todo form

--- src/app/todo/add-todo-drawer/add-todo-drawer.component.ts
+++ src/app/todo/add-todo-drawer/add-todo-drawer.component.ts
@@ -12,55 +12,59 @@ import { Router } from '@angular/router';
 })
 export class AddTodoDrawerComponent implements OnInit {
 
-  form!: FormGroup;
+  form = new FormGroup({
+    tasks: new FormArray<FormControl>([], { 
+      validators: [Validators.minLength(1)]
+    }),
+    tagSelector: new FormControl(),
+    taskInput: new FormControl<string | null>(null),
+    name: new FormControl(null, [Validators.required])
+  }) ;
   submitting: boolean = false;
-  dateToday!: string;
+  dateToday = moment().format('dddd, MMMM DD');
+
   constructor(private router: Router, private todoService: TodoService) {}
-
-  ngOnInit() {
-
-    this.form = new FormGroup({
-      tasks: new FormArray([] as any[], { 
-        validators: [Validators.minLength(1)]
-      }),
-      task: new FormControl(),
-      name: new FormControl(null, [Validators.required])
-    }) 
-
-    this.dateToday = moment().format('dddd, MMMM DD')
-  }
+  ngOnInit() {}
 
   addTask(e: any) {
+    const tasks = this.form.get('tasks') as FormArray
+    const taskInput = this.form.get('taskInput') as FormControl
+
     if(e?.keyCode == 13) {
       e?.preventDefault()
       const value = e?.target?.value;
       if(!value) return;
 
-      this.form.get('tasks')?.value?.push({ name: value, isCompleted: false })
-      this.form.get('task')?.setValue('')
+      tasks.push(new FormControl({ name: value, isCompleted: false }))
+      taskInput.setValue('')
     }
   }
 
   
   completeTask(index: number) {
-    const task = this.form.get('tasks')?.value[index]
+    const tasks = this.form.get('tasks') as FormArray
+
+    const task = tasks.value[index]
     if(task) {
       task.isCompleted = !task.isCompleted
     }
   }
 
   removeTask(index: number) {
-    
+    const tasks = this.form.get('tasks') as FormArray
+    tasks.removeAt(index)
   }
 
   handleSubmit() {
     this.submitting = true
 
-    const value = {
-      ...omit(this.form.value, ['task']),
+    let data = this.form.value;
+    this.todoService.saveTodo({
+      name: data.name,
+      tasks: data.tasks,
+      tags: data.tagSelector.values,
       createdAt: moment().toISOString()
-    }
-    this.todoService.saveTodo(value)
+    })
       .subscribe(() => {
         this.submitting = false
         this.router.navigate(['/'], { queryParams: { "refresh": true } })
